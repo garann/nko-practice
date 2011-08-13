77c9ca4eae78ac0319fb1181475e8ece7c5615e5
Garann Means <garann@gmail.com>
2011-08-13 18:55:12 -0500

    server.js (existing)

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-var express = require('express').createServer();
+var express = require('express').createServer(),
+	redis = require('redis-client'),
+	db = redis.createClient();
  
 express.get('/', function(req, res) {
 	res.send("I need like 17 clones.");
