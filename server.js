3c852915f545a7296b9126d74c2a6150f8132d77
Garann Means <garann@gmail.com>
2011-08-13 19:12:31 -0500

    added some basic socket.io stuff

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 var express = require('express').createServer(),
 	redis = require('redis-client'),
-	db = redis.createClient();
+	db = redis.createClient(),
+	io = require('socket.io'),
+	sock = io.listen(express);
  
 express.get('/', function(req, res) {
-	res.send("I need like 17 clones.");
+	res.render("index");
+});
+
+sock.on("connection",function(c) {
+	c.on("message",function(m) {
+		sock.broadcast(m);
+	});
 });
  
 express.listen(80);
