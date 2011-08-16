8fefd4bad05b9bbeaa302b5bb8915a25cb402389
phillyqueso <pjacobs@xeekr.com>
2011-08-16 14:07:03 -0500

    User data Object has been abstracted. Sessions now working with page reloads, etc. Form submit vs button click on talk stuff. Updated package.json, redis/hiredis is a go.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express'),
+        connect = require('connect'),
         config = require('./config.js').config,
+        User = require('./lib/User.js').User,
 	app = express.createServer(),
         redisStore = require('connect-redis')(express);
-	redis = require('redis-client'),
-	db = redis.createClient(),
         rest = require('restler'),
         io = require('socket.io').listen(app);
 
@@ -46,16 +46,27 @@ app.get('/', function(req, res) {
     res.render("index.html");
 });
 
+var getSID = function(client) {
+    var clientCookies = connect.utils.parseCookie(client.handshake.headers.cookie);
+    return clientCookies['connect.sid'];
+}
+
 /* SOCKETS */
 io.sockets.on('connection', function(client) {
 
     /* Handle session reconnect, page reloads, etc. */
+    client.sId = getSID(client);
+    if (users[client.sId]) {
+	client.emit('authPassed', {username: users[client.sId].username});
+    } else {
+	console.log("No user for session: "+client.sId);
+    }
 
     client.on('browserid', function(data) {
 	rest.post("https://browserid.org/verify", 
 		  { data: {assertion: data.assertion, audience: host} }).on('complete', function(bData, response) {
 		      if (bData.status == "okay" && bData.audience == host) {
-			  users[client.id] = data.username;
+			  users[client.sId] = new User(client.sId, bData.email, data.username);
 			  client.emit('authPassed', {username: data.username});
 		      } else {
 			  console.log("auth error");
@@ -64,8 +75,8 @@ io.sockets.on('connection', function(client) {
     });
 
     client.on('chatMsg', function(data) {
-	if (users[client.id] != "") {
-	    client.broadcast.emit('chatMsg', {nick: users[client.id], msg: data.chatMsg});
+	if (users[client.sId] != "") {
+	    client.broadcast.emit('chatMsg', {nick: users[client.sId].username, msg: data.chatMsg});
 	} else {
 	    console.log("user not logged in");
 	}
